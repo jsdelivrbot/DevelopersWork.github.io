602eb5aee0af89e45b2d64c75d0d584b48c135e7
DevelopersWork <gopinadh199922@gmail.com>
2017-04-22 03:52:34 +0530

    nav ok

---
+++ globalIncludes/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	
+    setTimeout(function(){
+		var width = screen.width,height = screen.height;
+		while(width>=16 && height>=9){
+			width=width/(16*16);
+			height=height/(9*16);
+			if(width != 5 && height != 5){
+				alert('This Page is Optimized for 16:9 Resolution. So,this page may not be working at best now');
+			}
+		}
+    },500);
+	
+	setTimeout(function(){
+		$("#loader").fadeOut("slow");
+	},5000);
+	
+	setTimeout(function() {
+        $("#navigationBar").show('fadeIn',{},500)
+    },5000);
+	
+});
