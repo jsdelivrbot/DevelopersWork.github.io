434aff2d2ea7bf29f4b3bc2e9817809a8cc4e0d0
DevelopersWork <gopinadh199922@gmail.com>
2017-04-26 03:57:18 +0530

    google ads

--- works/GUIDES/AndrodDeviceTree/script.js
+++ works/GUIDES/AndrodDeviceTree/script.js
@@ -1,6 +1,16 @@
+var count=1;
 
 function showhide(id){
 	var e = document.getElementById(id);
+
 	e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+	if(count==1){
+		document.getElementById('button'+id).innerHTML = '<button>Hide</button>';
+		count=2;
+	}
+	else{
+		document.getElementById('button'+id).innerHTML = '<button>Show</button>';
+		count=1;
+	}
 }
 
